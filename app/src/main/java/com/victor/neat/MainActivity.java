cc64a363f271fcdcb2691db3bab45f13250bffd8
Kidrapper <wambuivictor@students.must.ac.ke>
2022-03-21 15:27:28 +0300

    first commit

---
+++ app/src/main/java/com/victor/neat/MainActivity.java
@@ -0,0 +1,46 @@
+package com.victor.neat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+     Button btnLogout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        btnLogout = findViewById(R.id.btn_logout);
+
+        btnLogout.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseAuth currentUser = mAuth.getCurrentUser();
+        if (currentUser==null){
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
+    }
+    public void logout() {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+}
